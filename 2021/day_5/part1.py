e6592b92f707f8fbd44dd12a50f913671d9eda99
Arun G Nair <arungnair@gmail.com>
2021-12-26 15:07:02 +0530

    Day 5 - part 1 - wip

--- 2021/day_5/part1.py
+++ 2021/day_5/part1.py
@@ -12,22 +12,46 @@ with open('input', 'r') as input:
         elif y1 == y2:
             hlines.append([(int(x1),int(y1)),(int(x2),int(y2))])
 
-#print('Horizontal lines:', len(hlines), hlines)
-print('Vertical lines:', len(vlines))
+print('Horizontal lines:', len(hlines), hlines)
+print('Vertical lines:', len(vlines), vlines)
+points = []
+intersection = 0
 
 for line in vlines:
-    print('>>', line)
+    #print('vline>>', line)
     a,b = line
     x,y1 = a
     x,y2 = b
 
     if y2 > y1:
         for y in range(y1,y2):
-            if y != y1: line.append((x,y))
+            points.append((x,y))
+        points.append((x,y2))
     elif y1 > y2:
         for y in range(y1,y2,-1):
-            if y != y1: line.append((x,y))
-    print('<<', line)
-    break
+            points.append((x,y))
+        points.append((x,y2))
 
-print('Vertical lines:', len(vlines))
+assert (798,666) in points
+assert (628,339) in points
+print('Points', points, len(points))
+
+hpoints = []
+for line in hlines:
+    #print('hline >>', line)
+    a,b = line
+    x1,y = a
+    x2,y = b
+    if x2 > x1:
+        for x in range(x1,x2):
+            hpoints.append((x,y))
+        hpoints.append((x2,y))
+    elif x1 > x2:
+        for x in range(x1,x2,-1):
+            hpoints.append((x,y))
+        hpoints.append((x2,y))
+
+print(hpoints, len(hpoints))
+print('Intersections:', intersection)
+assert (700,444) in hpoints
+assert (376,128) in hpoints
